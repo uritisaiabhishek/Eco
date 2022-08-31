bc527743fe8e34804e22123a3a97bec28d5a032a
Uriti Sai abhishek <53622212+uritisaiabhishek@users.noreply.github.com>
2022-08-31 20:28:58 +0530

    edited settings

--- eco_app/settings.py
+++ eco_app/settings.py
@@ -123,9 +123,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/images')
-MEDIA_URL = '/assets/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
